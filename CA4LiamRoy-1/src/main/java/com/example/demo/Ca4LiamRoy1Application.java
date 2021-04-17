3d38e4f873173ff50584216ebe3f4c34e71b809e
liamr <liamr@192.168.1.93>
2021-04-17 19:00:28 +0100

    Added Connections to MySQL

---
+++ CA4LiamRoy-1/src/main/java/com/example/demo/Ca4LiamRoy1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca4LiamRoy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ca4LiamRoy1Application.class, args);
+	}
+
+}
